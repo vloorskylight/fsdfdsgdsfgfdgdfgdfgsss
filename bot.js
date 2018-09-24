5ac7fc5f9d07f8fc9ef98b884d726886adf0bf76
vloorskylight <42692733+vloorskylight@users.noreply.github.com>
2018-09-24 23:41:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,31 @@ client.on('ready', () => {
 });
 
 
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 10,
+        maxAge: 86400,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(":link: تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : يوم | عدد استخدامات الرابط : 10 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
 ////////////////////////////////////////////////////////////////////////////////////////
 client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('486288199925104650');
